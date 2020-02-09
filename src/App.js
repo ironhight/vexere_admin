ca3421f9c6e948152dc5689b532ff494a38b84bf
SharkNam <56241790+SharkNam@users.noreply.github.com>
2020-02-09 13:26:50 +0700

    refactor sidebar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
-// import "./index.css";
-// import "./App.css";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-// import Login from './components/Auth/Login'
-import LoginPage from "./pages/LoginPage";
-// import Navbar from "./components/Navbar/index";
-// import Manager from "./components/Manager/index";
 import jwtDecode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/actions/auth";
+
+//core component
+import LoginPage from "./pages/LoginPage";
+import Manager from "./pages";
+// import Navbar from "./components/Navbar/index";
+// import Manager from "./components/Manager/index";
 // import Profile from "./components/Profile";
 // import Trip from "./components/Manager/Trip/TripList";
 // import Station from "./components/Manager/Station/StationList";
 // import UpdateStation from "./components/Manager/Station/UpdateStation";
 // import CreateTrip from "./components/Manager/Trip/CreateTrip";
+// import TopNavigation from "./components/topNavigation";
+// import sideNavigation from "./components/topNavigation";
+// import Footer from "./components/Footer";
 
-import Manager from "./pages";
+//css
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import "./index.css";
-// import TopNavigation from "./components/topNavigation";
-// import sideNavigation from "./components/topNavigation";
-// import Footer from "./components/Footer";
 
 class App extends Component {
   constructor(props) {
@@ -56,7 +56,6 @@ class App extends Component {
     return (
       <div className="App">
         <BrowserRouter>
-          {/* <TopNavigation /> */}
           <Switch>
             <Route
               path="/"
